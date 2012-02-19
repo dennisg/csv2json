24a1dbe59fb9c7f3dbe8da180e9ef1cd0311cc53
Dennis Geurts <github@dennisg.nl>
2012-02-19 16:10:00 +0100

    added some documentation, and removed the default URL

--- test/test.js
+++ test/test.js
@@ -1,5 +1,10 @@
 var csv = require('../lib/csv');
 
-csv.parse(function(data){
+var url = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M2.5.txt';
+var opts = { url : url };
+
+
+csv.parse(function(err, data) {
 	console.log(data);
-});
+}, opts);
+
